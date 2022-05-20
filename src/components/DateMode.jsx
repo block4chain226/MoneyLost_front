ccd500aff7a8a1d9707e3b9370aac947726db1f8
Ivan <retouch226@gmail.com>
2022-05-20 16:23:09 +0300

    header, addExpenses, expensesDetails

--- src/components/DateMode.jsx
+++ src/components/DateMode.jsx
@@ -9,27 +9,33 @@ const DateMode = () => {
 
   return (
     <div className="date-mod__date">
-      <NeoButton
-        onClick={() => {
-          setDateMode({ day: true, month: false, year: false });
-        }}
-      >
-        Day
-      </NeoButton>
-      <NeoButton
-        onClick={() => {
-          setDateMode({ day: false, month: true, year: false });
-        }}
-      >
-        Mon
-      </NeoButton>
-      <NeoButton
-        onClick={() => {
-          setDateMode({ day: false, month: false, year: true });
-        }}
-      >
-        Year
-      </NeoButton>
+      <div className="header-btn-container">
+        <NeoButton
+          onClick={() => {
+            setDateMode({ day: true, month: false, year: false });
+          }}
+        >
+          Day
+        </NeoButton>
+      </div>
+      <div className="header-btn-container">
+        <NeoButton
+          onClick={() => {
+            setDateMode({ day: false, month: true, year: false });
+          }}
+        >
+          Mon
+        </NeoButton>
+      </div>
+      <div className="header-btn-container">
+        <NeoButton
+          onClick={() => {
+            setDateMode({ day: false, month: false, year: true });
+          }}
+        >
+          Year
+        </NeoButton>
+      </div>
     </div>
   );
 };
